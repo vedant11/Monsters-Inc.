7d3e882ae94ad7f0da98e81a2383e4a93ffcc1cb
Vedant Panchal <47473330+vedant11@users.noreply.github.com>
2019-12-25 22:33:49 +0530

    With Redux support

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import {createStore} from 'redux';
+
 import './index.css';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
 import App from './container/App';
+import {searchMonsters} from './reducers'
+import 'tachyons';
+import * as serviceWorker from './serviceWorker';
+
 import './fonts/PermanentMarker-Regular.ttf'
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+
+export const store=createStore(searchMonsters);
+
+ReactDOM.render(<Provider store={store}>
+                    <App/>
+                </Provider>,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
