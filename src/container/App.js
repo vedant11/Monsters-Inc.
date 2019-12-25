f53d3bb9a4282eaf714a7bd7140a68607a0c234c
Vedant Panchal <47473330+vedant11@users.noreply.github.com>
2019-12-25 22:33:27 +0530

    with Redux support

--- src/container/App.js
+++ src/container/App.js
@@ -1,7 +1,22 @@
 import React,{Component} from 'react';
+import { connect } from 'react-redux';
+
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
+import { searchField } from '../Action';
+
+const mapStateToProps=(state)=>{
+    return{
+        searchField: state.searchField              //we dont need to dot up into the reducers because we have only one reducer
+    }
+}
+
+const mapDispatchToProps=(dispatch)=>{
+    return{
+        onSearchChange:(event)=>dispatch(searchField(event.target.value))
+    }
+}
 
 
 class App extends Component{                    //defining the class
@@ -9,7 +24,6 @@ class App extends Component{                    //defining the class
         super()                                 //mandatory
         this.state={
             monsters:[],                        //kept empty to be edited later
-            searchField:''
             }
         }
 
@@ -19,12 +33,9 @@ class App extends Component{                    //defining the class
         .then(users=>{this.setState({monsters:users})});        //monsters state changed to that received
         }
 
-    onSearchChange=(event)=>{
-        this.setState({searchField:event.target.value})         //function called on change in input html element
-        }                                                        //||changes the searchField variable
-        
     render(){
-        const {monsters,searchField}=this.state;                //destructuring
+        const {monsters}=this.state;                //destructuring
+        const {searchField, onSearchChange}=this.props;             //searchField now comes as props due to Redux usage
         const newList=monsters.filter(monster=>{                //filtering monsters list according to search keyword input
         return monster.name.toLowerCase().includes(searchField.toLowerCase())
         })
@@ -33,7 +44,7 @@ class App extends Component{                    //defining the class
             <div>
                 <h1 id="title" className="tc bg-light-green pv5 ma0">Monsters,Inc.</h1>
                 <p className="tc">Search by name: </p>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <CardList monsters={newList}/>
                 </Scroll>
@@ -43,4 +54,4 @@ class App extends Component{                    //defining the class
     }
 
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
